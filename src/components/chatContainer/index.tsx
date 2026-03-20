8aeb0db31bd6c6df86b1dbdfebc5601c0375c129
christophe gabriel <christopheguerra67@gmail.com>
2026-03-20 10:29:09 -0300

    feat:adding some changes in chat container

--- src/components/chatContainer/index.tsx
+++ src/components/chatContainer/index.tsx
@@ -1,32 +1,76 @@
 import type { FormEvent } from "react";
+import "./style.css";
 
-export default function ChatContainer() { 
+export default function ChatContainer() {
     return (
-        <div className="w-full h-screen flex flex-col flex-1   items-center ">
-            <div className="relative flex flex-col justify-center h-screen bg-red-500 px-5 w-full max-w-[80%]">
+        <div className="w-full  flex flex-col flex-1  overflow-hidden items-center ">
+            <div className="relative flex flex-col justify-center h-screen  px-5 w-full max-w-[80%]">
 
                 {/* Container das mensagens */}
-                <div className="w-full  flex flex-col">
-
-                    <section className="flex-1 overflow-y-auto  pb-24">
-                        {/* messages */}
+                <div className="message-container ">
+                    <section className="message-list w-full h-full overflow-auto mb-2">
+                        <ul className="flex flex-col justify-end w-full h-full gap-2">
+                            <li className=" w-full  flex justify-end">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-blue-500">
+                                        <p>teste de mensagem enviada : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                            <li className=" w-full  flex justify-end">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-blue-500">
+                                        <p>teste de mensagem enviada : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                            <li className=" w-full  flex justify-end">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-blue-500">
+                                        <p>teste de mensagem enviada : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                            <li className=" w-full  flex justify-end">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-blue-500">
+                                        <p>teste de mensagem enviada : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                            <li className=" w-full  flex justify-start">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-white">
+                                        <p>teste de mensagem recebida : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                             <li className=" w-full  flex justify-start">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-white">
+                                        <p>teste de mensagem recebida : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                             <li className=" w-full  flex justify-start">
+                                {/* conteudo */}
+                                    <div className="max-w-[80%] w-fit p-2  bg-white">
+                                        <p>teste de mensagem recebida : Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, mollitia asperiores similique repellat repellendus accusamus esse voluptatem id explicabo in voluptate sequi temporibus suscipit earum perferendis neque nesciunt dicta quis!</p>
+                                    </div>
+                            </li>
+                            
+                        </ul>
+                        {/* array de  messages */}
                     </section>
-
                 </div>
 
                 {/* Input FIXO */}
-                <section className=" w-full ">
-                    <div className="backdrop-blur-md bg-white/70 rounded-full p-2">
+                <section className=" w-full mb-5">
+                    <div className="backdrop-blur-md bg-white/70 rounded-full w-full p-1">
                         <form
                             onSubmit={(e: FormEvent) => e.preventDefault()}
                             className="flex gap-2"
                         >
                             <input
                                 type="text"
-                                className="flex-1 p-2 rounded outline-none"
+                                className="flex-1 px-4 py-1 rounded outline-none"
                                 placeholder="Digite sua mensagem..."
                             />
-                            <button type="submit" className="bg-(--button-primary) px-4 rounded-full">
+                            <button type="submit" className="bg-(--button-primary) text-white px-4 rounded-full">
                                 Enviar
                             </button>
                         </form>
